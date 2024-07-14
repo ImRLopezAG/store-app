79fed1931477b29bd97cbc0474b96420031b100f
Angel Gabriel Lopez TSX <angel178293lopez@gmail.com>
2024-07-14 19:18:04 -0400

    feat: Add cart, card, and customer services and state

---
+++ src/services/cart/service.ts
@@ -0,0 +1,28 @@
+export const cartService = () => {
+  const getProductVAT = (product: Item) => product.price.fullPrice * 0.18;
+  const getProductTotalExcludingVAT = (product: Item) =>
+    product.price.fullPrice * product.quantity;
+
+  const round2Decimals = (num: number) => Number(num.toFixed(2));
+  const getProductTotal = (product: Item) => {
+    const { price } = product;
+    return round2Decimals(
+      price.fullPrice * product.quantity +
+        getProductVAT(product) * product.quantity
+    );
+  };
+
+  const f = (value: number) =>
+    new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+    }).format(value);
+
+  return {
+    getProductVAT,
+    getProductTotalExcludingVAT,
+    round2Decimals,
+    getProductTotal,
+    f,
+  };
+};
---
+++ src/services/cart/state.ts
@@ -0,0 +1,38 @@
+export const emptyCustomer: Customer = {
+  name: '',
+  email: '',
+  address: '',
+  city: '',
+  state: '',
+  zip: Math.floor(Math.random() * 100000).toString(),
+  country: '',
+  phone: '',
+  password: '',
+};
+export const emptyCard: Card = {
+  number: '',
+  cvc: '',
+  month: '',
+  year: '',
+  holder: 'Visa',
+};
+export const emptyCart: Cart = {
+  id: `INV-${Math.floor(Math.random() * 100000).toString()}`,
+  payment: {
+    customer: emptyCustomer,
+    card: emptyCard,
+  },
+  lines: [],
+  totals: {
+    products: 0,
+    subtotal: 0,
+    totalVAT: 0,
+    total: 0,
+    shipping: 0,
+  },
+  createdAt: new Intl.DateTimeFormat('en-US', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  }).format(new Date()),
+};
