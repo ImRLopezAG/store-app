fbe4c17a47ffeb85b2e0c5cc8b2962dac329c2f5
Angel Gabriel Lopez TSX <angel178293lopez@gmail.com>
2024-07-14 19:19:06 -0400

    feat: Update home page to display cart information

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,10 +1,12 @@
 import { Text } from '@ui/text';
 import { Wrapper } from '@ui/wrapper';
+import { useCart } from '@hooks/use-cart'
 
 export default function Home() {
+  const { cart } = useCart()
   return (
     <Wrapper title='Home'>
-      <Text text='Home' />
+      <Text text={`Cart: ${cart?.id}`} />
     </Wrapper>
   );
 }
