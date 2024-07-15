9123b4cd940d9fdb67146e33258b68bcc47308c9
Angel Gabriel Lopez TSX <angel178293lopez@gmail.com>
2024-07-14 21:37:06 -0400

    refactor: Update Wrapper component to use forwardRef for better performance

--- src/app/_components/ui/wrapper.tsx
+++ src/app/_components/ui/wrapper.tsx
@@ -1,22 +1,23 @@
 import { cn } from '@shared/cn';
 import { Container } from '@ui/container';
 import { Stack } from 'expo-router';
-import { View } from 'react-native';
+import { forwardRef } from 'react';
+import { StyleProp, View, ViewProps } from 'react-native';
 interface WrapperProps {
   children: React.ReactNode;
   title: string;
   className?: string;
 }
 
-export const Wrapper: React.FC<WrapperProps> = ({
-  children,
-  title,
-  className,
-}) => {
-  return (
-    <Container>
-      <Stack.Screen options={{ title }} />
-      <View className={cn('p-4', className)}>{children}</View>
-    </Container>
-  );
-};
+export const Wrapper: React.FC<WrapperProps & ViewProps> = forwardRef(
+  ({ children, title, className, ...props }, ref) => {
+    return (
+      <Container>
+        <Stack.Screen options={{ title }} />
+        <View className={cn('p-4', className)} {...props}>
+          {children}
+        </View>
+      </Container>
+    );
+  }
+);
